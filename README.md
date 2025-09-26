2c0938178305f24b9407f11f4048c8b63b0cb8c1
LeirBaGMC <gabominda@outlook.com>
2025-09-26 12:05:57 -0500

    Primer commit: API minima con FastAPI y DB quemada

---
+++ README.md
@@ -0,0 +1,10 @@
+
+#API REST mínima - Login  demo
+API minima con FastAPI que valida usuario/contraseña contra una lista de usuarios
+
+## Ejecutar servidor
+
+```bash
+
+pip install -r requirements.txt
+
---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from sqlmodel import SQLModel
+
+app = FastAPI(title="API REST mínima - Login demo")
+
+class User:
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+
+class UserRepository:
+    def __init__(self):
+        self.usuarios = [
+            User("alice", "123"),
+            User("bob", "456")
+        ]
+
+    def autenticar(self, username: str, password: str):
+        for u in self.usuarios:
+            if u.username == username and u.password == password:
+                return u
+        return None
+
+repo = UserRepository()
+
+# --- Endpoints ---
+@app.get("/")
+def read_root():
+    return {"mensaje": "Hola, FastAPI funciona 🚀"}
+
+@app.post("/login")
+def login(username: str, password: str):
+    user = repo.autenticar(username, password)
+    if user:
+        return {"ok": True, "msg": f"Bienvenido {user.username}"}
+    return {"ok": False, "msg": "Usuario o contraseña inválidos"}
